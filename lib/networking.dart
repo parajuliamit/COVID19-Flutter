a3ed290489b8ed36e0e2f8593df2694b8cad3bd4
parajuliamit <be2017se587@gces.edu.np>
2020-05-13 19:50:28 +0545

    Initial commit

---
+++ lib/networking.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class UpdateNumbers {
+  static const _api_key = '5a05253a6fmsh2f021035f56a7ddp1ef4e1jsn46d114c90a57';
+  static const String _baseUrl = "covid-19-data.p.rapidapi.com";
+  static const Map<String, String> _headers = {
+    "content-type": "application/json",
+    "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+    "x-rapidapi-key": _api_key,
+  };
+
+  Future<List<String>> get(String country) async {
+    List<String> data = [];
+    http.Response response;
+    if (country == 'Global') {
+      Uri uri = Uri.https(_baseUrl, "/totals", {"format": "json"});
+
+      response = await http.get(uri, headers: _headers);
+    } else {
+      Uri uri = Uri.https(
+          _baseUrl, "/country/code", {"format": "json", "code": country});
+
+      response = await http.get(uri, headers: _headers);
+    }
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      print(country);
+      print(response.body);
+      if (decodedData[0]['lastUpdate'] == null) {
+        return ['--', '--', '--', '--', '--'];
+      }
+      ;
+      try {
+        int confirmed = decodedData[0]['confirmed'];
+        int deaths = decodedData[0]['deaths'];
+        int recovered = decodedData[0]['recovered'];
+        int active = confirmed - deaths - recovered;
+
+        data.add(decodedData[0]['lastUpdate']);
+        data.add(confirmed.toString());
+        data.add(deaths.toString());
+        data.add(active.toString());
+        data.add(recovered.toString());
+      } catch (e) {
+        print(e);
+        return ['--', '--', '--', '--', '--'];
+      }
+    } else {
+      print(response.statusCode);
+      data = ['--', '--', '--', '--', '--'];
+    }
+    return data;
+  }
+}
